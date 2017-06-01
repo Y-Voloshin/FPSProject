9af0bc5cb7648f5d0ef1a97f62e84365463aa2c1
Y-Voloshin <voloshin-y@yandex.ru>
2017-06-01 21:29:57 +0300

    Revert "19 Cursor fix"
    This reverts commit 6dd51f2ab1a84c9925fa67a5a584b71ea48726e0.

--- Assets/3rdParty/Standard Assets/Scripts/MouseLook.cs
+++ Assets/3rdParty/Standard Assets/Scripts/MouseLook.cs
@@ -19,6 +19,7 @@ namespace VGF.FromUnityStandartAssets
 
         private Quaternion m_CharacterTargetRot;
         private Quaternion m_CameraTargetRot;
+        private bool m_cursorIsLocked = true;
 
         public void Init(Transform character, Transform camera)
         {
@@ -54,6 +55,8 @@ namespace VGF.FromUnityStandartAssets
                 character.localRotation = m_CharacterTargetRot;
                 camera.localRotation = m_CameraTargetRot;
             }
+
+            UpdateCursorLock();
         }
         
         public void SetCursorLock(bool value)
@@ -64,7 +67,37 @@ namespace VGF.FromUnityStandartAssets
                 Cursor.lockState = CursorLockMode.None;
                 Cursor.visible = true;
             }
-        }        
+        }
+
+        public void UpdateCursorLock()
+        {
+            //if the user set "lockCursor" we check & properly lock the cursos
+            if (lockCursor)
+                InternalLockUpdate();
+        }
+
+        private void InternalLockUpdate()
+        {
+            if(Input.GetKeyUp(KeyCode.Escape))
+            {
+                m_cursorIsLocked = false;
+            }
+            else if(Input.GetMouseButtonUp(0))
+            {
+                m_cursorIsLocked = true;
+            }
+
+            if (m_cursorIsLocked)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+            else if (!m_cursorIsLocked)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+        }
 
         Quaternion ClampRotationAroundXAxis(Quaternion q)
         {
--- Assets/VGF/Scripts/CursorExtensions.cs
+++ Assets/VGF/Scripts/CursorExtensions.cs
@@ -14,7 +14,6 @@ namespace VGF
             
             Cursor.lockState = hide? CursorLockMode.Locked : CursorLockMode.None;
             Cursor.visible = !hide;
-            Debug.Log(string.Format("{0}  {1}  {2}", hide, Cursor.lockState, Cursor.visible));
         }
     }
 }
